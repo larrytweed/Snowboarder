0acb5dcd49d21fca6fe34ee2a744a838f202fd0c
Larry Tweed <larrytweed@gmail.com>
2022-10-28 16:35:36 -0700

    Added Loadscene for crash and finish triggers

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
@@ -8,7 +9,7 @@ public class CrashDetector : MonoBehaviour
     {
         if (other.tag == "Ground")
         {
-            Debug.Log("You crashed!");
+            SceneManager.LoadScene(0);
         }
     }
     // Start is called before the first frame update
